eeeaa64617b78b6e6fadc1e38637630d66bdfe4e
Gabriel <mzsgabriel@gmail.com>
2023-06-30 15:52:25 -0300

    Tabelas

---
+++ TestesDonaMarianaDb/dbo/Tables/TBAlternativa.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[TBAlternativa] (
+    [ID]         INT          NOT NULL,
+    [Gabarito]   BIT          NOT NULL,
+    [ID_Questao] INT          NOT NULL,
+    [Descricao]  VARCHAR (50) NOT NULL,
+    CONSTRAINT [PK_Alternativa] PRIMARY KEY CLUSTERED ([ID] ASC),
+    CONSTRAINT [FK_Alternativa_QuestaoDB] FOREIGN KEY ([ID_Questao]) REFERENCES [dbo].[TBQuestao] ([ID])
+);
+
---
+++ TestesDonaMarianaDb/dbo/Tables/TBMateria.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[TBMateria] (
+    [ID]            INT          NOT NULL,
+    [nome]          VARCHAR (50) NOT NULL,
+    [serie]         VARCHAR (50) NOT NULL,
+    [ID_Disciplina] INT          NOT NULL,
+    CONSTRAINT [PK_MateriaDB] PRIMARY KEY CLUSTERED ([ID] ASC),
+    CONSTRAINT [FK_MateriaDB_DisciplinaTB] FOREIGN KEY ([ID_Disciplina]) REFERENCES [dbo].[TBDisciplina] ([ID])
+);
+
