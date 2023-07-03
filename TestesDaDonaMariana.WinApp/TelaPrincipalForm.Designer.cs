a4eaea1ad9d8536888c5eeae20508f3cb0ff8bfc
Gabriel <mzsgabriel@gmail.com>
2023-07-03 16:01:59 -0300

    Tela Principal

---
+++ TestesDaDonaMariana.WinApp/TelaPrincipalForm.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace TestesDaDonaMariana.WinApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Disciplinas = new System.Windows.Forms.Button();
+            this.btn_Materias = new System.Windows.Forms.Button();
+            this.btn_Questoes = new System.Windows.Forms.Button();
+            this.btn_Testes = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_Disciplinas
+            // 
+            this.btn_Disciplinas.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_Disciplinas.Location = new System.Drawing.Point(215, 88);
+            this.btn_Disciplinas.Name = "btn_Disciplinas";
+            this.btn_Disciplinas.Size = new System.Drawing.Size(175, 130);
+            this.btn_Disciplinas.TabIndex = 0;
+            this.btn_Disciplinas.Text = "Disciplinas";
+            this.btn_Disciplinas.UseVisualStyleBackColor = true;
+            // 
+            // btn_Materias
+            // 
+            this.btn_Materias.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_Materias.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btn_Materias.Location = new System.Drawing.Point(396, 88);
+            this.btn_Materias.Name = "btn_Materias";
+            this.btn_Materias.Size = new System.Drawing.Size(175, 130);
+            this.btn_Materias.TabIndex = 1;
+            this.btn_Materias.Text = "Matérias";
+            this.btn_Materias.UseVisualStyleBackColor = true;
+            // 
+            // btn_Questoes
+            // 
+            this.btn_Questoes.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_Questoes.Location = new System.Drawing.Point(215, 224);
+            this.btn_Questoes.Name = "btn_Questoes";
+            this.btn_Questoes.Size = new System.Drawing.Size(175, 130);
+            this.btn_Questoes.TabIndex = 2;
+            this.btn_Questoes.Text = "Questões";
+            this.btn_Questoes.UseVisualStyleBackColor = true;
+            // 
+            // btn_Testes
+            // 
+            this.btn_Testes.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btn_Testes.Location = new System.Drawing.Point(396, 224);
+            this.btn_Testes.Name = "btn_Testes";
+            this.btn_Testes.Size = new System.Drawing.Size(175, 130);
+            this.btn_Testes.TabIndex = 3;
+            this.btn_Testes.Text = "Testes";
+            this.btn_Testes.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(215, 88);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(175, 130);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Disciplinas";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button2.Location = new System.Drawing.Point(215, 224);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(175, 130);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Questões";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button3.Font = new System.Drawing.Font("Segoe UI", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.button3.Location = new System.Drawing.Point(396, 224);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(175, 130);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Testes";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 461);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.btn_Testes);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btn_Questoes);
+            this.Controls.Add(this.btn_Materias);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.btn_Disciplinas);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "Testes da Dona Mariana";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Button btn_Disciplinas;
+        private Button btn_Materias;
+        private Button btn_Questoes;
+        private Button btn_Testes;
+        private Button button1;
+        private Button button2;
+        private Button button3;
+    }
+}
