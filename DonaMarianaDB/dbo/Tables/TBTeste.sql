8291317e1cf190786dcfdc94f21d85b3670ac119
Gabriel <mzsgabriel@gmail.com>
2023-07-06 02:25:14 -0300

    Atualização Tabela Teste

--- DonaMarianaDB/dbo/Tables/TBTeste.sql
+++ DonaMarianaDB/dbo/Tables/TBTeste.sql
@@ -1,11 +1,14 @@
 ﻿CREATE TABLE [dbo].[TBTeste] (
-    [ID]              INT  NOT NULL IDENTITY,
-    [Numero_Questoes] INT  NOT NULL,
-    [Materia_ID]      INT  NULL,
-    [Data_Criacao]    DATE NOT NULL,
-    [Disciplina_ID]   INT  NOT NULL,
+    [ID]              INT          IDENTITY (1, 1) NOT NULL,
+    [Numero_Questoes] INT          NOT NULL,
+    [Materia_ID]      INT          NULL,
+    [Data_Criacao]    DATE         NOT NULL,
+    [Disciplina_ID]   INT          NOT NULL,
+    [Titulo]          VARCHAR (50) NOT NULL,
     CONSTRAINT [PK_Teste] PRIMARY KEY CLUSTERED ([ID] ASC),
     CONSTRAINT [FK_Teste_DisciplinaTB] FOREIGN KEY ([Disciplina_ID]) REFERENCES [dbo].[TBDisciplina] ([ID]),
     CONSTRAINT [FK_Teste_MateriaDB] FOREIGN KEY ([Materia_ID]) REFERENCES [dbo].[TBMateria] ([ID])
 );
+
+
 
